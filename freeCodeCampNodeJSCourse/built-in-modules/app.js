596aa8c72384ee8aa01bace396d352648b391d5e
rukiyeaslan <19rasln@gmail.com>
2023-01-22 04:18:02 +0300

    fcc course built-in modules

--- freeCodeCampNodeJSCourse/built-in-modules/app.js
+++ freeCodeCampNodeJSCourse/built-in-modules/app.js
@@ -1,3 +1,16 @@
-const {readFileSync} = require('fs')
+const {readFileSync, writeFileSync} = require('fs')
 // same as const readFileSync = require('fs') 
 
+//read from the input file in a specified format
+const first = readFileSync('../content/first.txt', 'utf8')
+const second = readFileSync('../content/second.txt', 'utf8')
+console.log(first, second)
+
+//write the content in the first and second file into a file named result.txt
+//if result.txt is not created, node will create it
+//third object is a flag, a means append
+writeFileSync(
+    '../content/result.txt',
+    `here is the result: ${first}, ${second}`,
+    {flag: 'a'}
+    )
