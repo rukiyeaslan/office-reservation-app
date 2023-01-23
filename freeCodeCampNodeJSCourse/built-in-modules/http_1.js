916f20bde2d31b54a2b58b682e4241e0cd3b0141
rukiyeaslan <19rasln@gmail.com>
2023-01-23 19:20:39 +0300

    http module introp

--- freeCodeCampNodeJSCourse/built-in-modules/http_1.js
+++ freeCodeCampNodeJSCourse/built-in-modules/http_1.js
@@ -1,7 +1,6 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    //console.log(req)
     if(req.url === '/'){
         res.end(`<h1>Welcome to our our home page</h1>
                     <p>to learn more about us <a href="/about">click</a> here</p>`)
@@ -15,7 +14,8 @@ const server = http.createServer((req, res) => {
         <p>We can't seem to find page that you are looking for</p>
         <a href="/">Back home page</a>`
     )
-        }
-})
+    }
+}).listen(8080) //on port 8080
 
-server.listen(8080)
+// an alternative to .listen() method usage at line 18
+// server.listen(8080)
---
+++ freeCodeCampNodeJSCourse/built-in-modules/npm_general_info.md
@@ -0,0 +1,15 @@
+# npm
+
+global command comes with node
+
+### npm --version
+Prints the current version of npm
+
+## local dependency
+Use it only in this particular project
+* npm i <packageName>
+
+## global dependency 
+Use it in any project
+* npm install -g <packageName>
+* sudo install -g <packageName> (mac)
