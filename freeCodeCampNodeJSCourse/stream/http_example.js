0f614e6a449e0554ef5a5085751cf74959fd6f85
rukiyeaslan <19rasln@gmail.com>
2023-01-24 15:01:01 +0300

    stream section completed

--- freeCodeCampNodeJSCourse/stream/http_example.js
+++ freeCodeCampNodeJSCourse/stream/http_example.js
@@ -2,29 +2,25 @@ var http = require('http')
 var fs = require('fs')
 const {writeFile, appendFile} = require('fs').promises
 
+//creating a big file
 writeFile('./content/big.txt', "", (err, result) => {
     if(err){
         console.log(err)
     }
-    //console.log(result)
 })
+
 for(let i = 0; i<10000; i++){
-    //console.log('here')
     appendFile('./content/big.txt', `So the line is: ${i}\n`, (err, result) => {
         if(err){
             console.log(err)
         }
-        //console.log(result)
     })}
 
 http.createServer(function (req, res) {
-//    const text = fs.readFileSync('./content/big.txt', 'utf8')
-//    res.end(text)
-
     //if we can read data in chunks, then we can write data in chunks, too
    const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
    fileStream.on('open', () => {
-        fileStream.pipe(res)
+        fileStream.pipe(res) //after adding this, transfer-encoding under response headers becomes "chunked"
    })
    fileStream.on('error', (err) => {
     res.end(err)
